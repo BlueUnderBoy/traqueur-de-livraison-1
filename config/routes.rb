d7bb859a540b9f0ffa856a2f20d88df0014c402a
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-01-17 19:39:51 +0000

    met a jour

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   #------------------------------
 
   # Routes for the User resource:
-  root to: "users#loreg"S
+  root to: "users#loreg"
 
   get("/users/sign_up", {:controller => "users", :action => "sup"})
 
